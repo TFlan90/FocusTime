5eabc8eb313dfa4a54317ed2bf23d4171ae38581
TDFlanley <tomflanleycs@gmail.com>
2025-05-14 14:44:17 -0700

    Added hr/mim/sec frames and labels

--- FocusTime.py
+++ FocusTime.py
@@ -1,11 +1,50 @@
 import tkinter as tk
+from tkinter import ttk
+import time
 
 
 root = tk.Tk()
 root.title("Focus Time")
+root.geometry("500x300")
+root.configure(bg="yellow")
 
-label = tk.Label(root, text = "This will be a clock")
-label.pack(padx=175, pady=175)
+hr_frame = ttk.Frame(root)
+hr_frame.pack(side=tk.LEFT)
+
+hr_label = ttk.Label(hr_frame,
+                     text = "00",
+                     font = ("Comic Sans", 50),
+                     foreground= "white",
+                     background = "purple",
+                     anchor = "center")
+hr_label.pack()
+
+minute_frame = ttk.Frame(root)
+minute_frame.pack(side=tk.LEFT)
+
+minute_label = ttk.Label(minute_frame,
+                     text = "00",
+                     font = ("Comic Sans", 50),
+                     foreground= "white",
+                     background = "green",
+                     anchor = "center")
+minute_label.pack()
+
+sec_frame = ttk.Frame(root)
+sec_frame.pack(side=tk.LEFT)
+
+sec_label = ttk.Label(sec_frame,
+                     text = "00",
+                     font = ("Comic Sans", 50),
+                     foreground= "white",
+                     background = "blue",
+                     anchor = "center")
+sec_label.pack()
+
+
+
+#label = tk.Label(root, text = "This will be a clock, one day")
+#label.pack(padx=175, pady=175)
 
 
 root.mainloop()
