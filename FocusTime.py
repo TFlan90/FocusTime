4f55daaf883a7033b433f61feff76f7b327a7b11
TDFlanley <tomflanleycs@gmail.com>
2025-05-20 23:18:00 -0700

    Added AM/PM clock function

--- FocusTime.py
+++ FocusTime.py
@@ -2,12 +2,38 @@ import tkinter as tk
 from tkinter import ttk
 import time
 
+# Function to update the clock labels
+def update_clock():
+    # Get current time
+    current_time = time.localtime()
+    if int(time.strftime("%H", current_time)) < 13:
+        hours = time.strftime("%H", current_time)
+    else:
+         hours = str(int(time.strftime("%H", current_time)) - 12)
+    minutes = time.strftime("%M", current_time)
+    seconds = time.strftime("%S", current_time)
 
+    # Update label text
+    hr_label.config(text=hours)
+    minute_label.config(text=minutes)
+    sec_label.config(text=seconds)
+    if int(time.strftime("%H", current_time)) > 12:
+        ampm_label.config(text="PM")
+    else:
+        ampm_label.config(text="AM")
+
+    # Schedule the update_clock function to be called after 1000ms 
+    root.after(1000, update_clock)
+
+
+#main window
 root = tk.Tk()
 root.title("Focus Time")
 root.geometry("500x300")
 root.configure(bg="yellow")
 
+
+#Frame and label for hours
 hr_frame = ttk.Frame(root)
 hr_frame.pack(side=tk.LEFT)
 
@@ -19,6 +45,7 @@ hr_label = ttk.Label(hr_frame,
                      anchor = "center")
 hr_label.pack()
 
+#Frame and label for minutes
 minute_frame = ttk.Frame(root)
 minute_frame.pack(side=tk.LEFT)
 
@@ -30,6 +57,7 @@ minute_label = ttk.Label(minute_frame,
                      anchor = "center")
 minute_label.pack()
 
+#Frame and label for seconds
 sec_frame = ttk.Frame(root)
 sec_frame.pack(side=tk.LEFT)
 
@@ -41,10 +69,17 @@ sec_label = ttk.Label(sec_frame,
                      anchor = "center")
 sec_label.pack()
 
+#Frame and label for AM/PM
+ampm_frame = ttk.Frame(root)
+ampm_frame.pack(side=tk.LEFT)
 
+ampm_label = ttk.Label(ampm_frame,
+                     text = "AM",
+                     font = ("Comic Sans", 50),
+                     foreground= "white",
+                     background = "Red",
+                     anchor = "center")
+ampm_label.pack()
 
-#label = tk.Label(root, text = "This will be a clock, one day")
-#label.pack(padx=175, pady=175)
-
-
+update_clock()
 root.mainloop()
